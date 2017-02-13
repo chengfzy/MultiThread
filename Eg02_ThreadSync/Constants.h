dd21f263ef5ebebf527956bfcdef4a95559a3497
chengfzy <chengcheng0829@gmail.com>
2017-02-13 14:29:34 +0800

    Create Constants.h

---
+++ Eg02_ThreadSync/Constants.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <process.h>
+using namespace std;
+
+long g_nNum;					//全局资源
+const int THREAD_NUM = 10;		//子线程个数
+
+//关键段, Critical Section
+CRITICAL_SECTION g_csThreadParameter, g_csThreadCode;
+
+//事件
+HANDLE g_hThreadEvent;
